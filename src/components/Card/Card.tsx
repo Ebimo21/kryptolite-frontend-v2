e3db3b8a09ae277f04f1e75b2027749a852c4561
ebimo21 <ebi4jah15@gmail.com>
2022-09-18 01:31:17 +0100

    mod launchpage

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,15 +4,17 @@ type Props = {
     classname?: string,
     title: string,
     content: String,
-    src: string
+    src: string,
+    id: number
   }
 
-const Card = ({title, content, src}: Props) => {
+const Card = ({title, content, src, id}: Props) => {
   return (
-    <div className="lg:mt-0 mt-44 bg-[#F7F7F7] text-center shadow-[0_4px_10px_2px_rgba(0,0,0,0.25)] p-4 basis-1/5  h-[22rem]">
+    <div className={`max-w-[500px] lg lg:mt-0 ${id!==1 ? 'mt-44': ''} bg-[#F7F7F7] text-center shadow-[0_4px_10px_2px_rgba(0,0,0,0.25)] p-4 basis-1/5  h-[22rem]`}>
         <div className='relative top-[-100px]'>
-          <span className={`m-auto clip-image h-[250px]   w-[250px]  block bg-cover bg-center ${src}  `}></span>
+          <span className={`m-auto clip-image h-[250px]   w-[250px]  block bg-cover bg-center ${src} `}></span>
           <h3>{title}</h3>
+          
           <p className="text-base ">{content}
           </p>
         </div>
--- src/pages/launchpad.tsx
+++ src/pages/launchpad.tsx
@@ -19,8 +19,8 @@ export default function launchpad(){
         to-[#29004E]/30 !max-w-none"
       >
         <Section className="text-white text-center ">
-          <h1 className="text-6xl font-black">KRYPTOLITE BAB CLUB</h1>
-          <p className="text-[#D2F804] max-w-[820px] text-[29px]">
+          <h1 className="text-4xl lg:text-6xl font-black">KRYPTOLITE BAB CLUB</h1>
+          <p className="text-xl text-[#D2F804] max-w-[820px] lg:text-[29px]">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque nibh tellus praesent tortor condimentum.
           </p>
           
@@ -31,13 +31,13 @@ export default function launchpad(){
       </Section>
 
       <Section  containerClass="bg-[#dedede40]">
-        <Section padding className="flex gap-10 items-stretch">
-          <figure ><img width="444px" height="444px" className={`bg-[#FFFFFF] shadow-[0_4px_10px_2px_rgba(0,0,0,0.25)]`}  src="./images/monkey.png" alt="Album"/></figure>
-          <div className="card-body">
-              <h2 className="card-title text-[48px]">Ultricies molestie.</h2>
-              <p className="text-[24px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa convallis cras viverra mauris purus. </p>
+        <Section padding className="flex flex-col md:flex-row gap-10 items-stretch">
+          <figure ><img width="100%" max-width="244px" height="444px" className={`max-w-[244px] m-auto block md:max-w-[444px] bg-[#FFFFFF] shadow-[0_4px_10px_2px_rgba(0,0,0,0.25)]`}  src="./images/monkey.png" alt="Album"/></figure>
+          <div className="card-body md:max-w-[600px]">
+              <h2 className="card-title text-[32px] md:text-[48px] text-center md:text-left">Ultricies molestie.</h2>
+              <p className="text-base text-center md:text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa convallis cras viverra mauris purus. </p>
               <div className="card-actions justify-end">
-                <Link to="/swap" as="button" className="inline-flex space-x-2 rounded-none mt-20 ">
+                <Link to="/swap" as="button" className="block md:inline-flex w-[256px] space-x-2 rounded-none m-auto mt-20 ">
                   <span>MINT YOUR NFT NOW</span>
                 </Link>
               </div>
@@ -46,10 +46,11 @@ export default function launchpad(){
       </Section>
 
       <Section>
+        <h1 className="text-center p-10">Our Top Product</h1>
         <Section padding className="lg:flex-row flex-col flex flex-wrap justify-around  items-center ">
             {cardItems.map(item =>{
                 return (
-                    <Card key={item.id} title={item.title} content={item.content} src={item.src}></Card>
+                    <Card key={item.id} id={item.id} title={item.title} content={item.content} src={item.src}></Card>
             )
             })}
         </Section>
